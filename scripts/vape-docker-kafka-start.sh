b3d769d834ca9635f7e7251cfc291ab2dad7d31a
BenxinNiu <benxin.niu@mun.ca>
2020-01-12 18:19:22 -0330

    enable cluster mode for zookeeper

--- scripts/vape-docker-kafka-start.sh
+++ scripts/vape-docker-kafka-start.sh
@@ -10,16 +10,22 @@ if [ "$len" == 0 ]; then
 fi
 
 if [ "$1" == zookeeper ]; then
-  echo running zookeeper using config vapeZooKeeperConfig/zookeeper.properties
-  sleep 5
-  sh ./vape/kafka/bin/zookeeper-server-start.sh ./vape/resources/vapeZooKeeperConfig/zookeeper.properties
+  if [ "$len" -le 1 ]; then
+    echo no enough args provided exiting ......
+    exit 1
+  else
+    echo running zookeeper using config vapeZooKeeperConfig/zookeeper.properties
+    sleep 2
+    echo "$2" >> /tmp/zookeeper/myid
+    sh ./vape/kafka/bin/zookeeper-server-start.sh ./vape/resources/vapeZooKeeperConfig/zookeeper.properties
+  fi
 elif [ "$1" == broker ]; then
   if [ "$len" -le 1 ]; then
     echo no enough args provided exiting ......
     exit 1
   else
     echo running broker using config vapeBrokerClusterConfig/broker"$2".properties
-    sleep 5
+    sleep 2
     sh ./vape/kafka/bin/kafka-server-start.sh ./vape/resources/vapeBrokerClusterConfig/broker"$2".properties
   fi
 else
