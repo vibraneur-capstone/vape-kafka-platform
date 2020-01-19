903be138ca991d41172300957b9ee6e4a980624b
BenxinNiu <benxin.niu@mun.ca>
2020-01-19 12:22:17 -0330

    create the script for running local setup

---
+++ scripts/vape-local-kafka-start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -rf local-kafka
+
+sh scripts/vape-local-kafka-zoo-keeper-start.sh
+sleep 10
+sh scripts/vape-local-kafka-broker-start.sh
+sleep 10
+
+# create test topic
+osascript 2>/dev/null <<EOF
+    tell application "System Events"
+      tell process "Terminal" to keystroke "t" using command down
+    end
+    tell application "Terminal"
+      activate
+      do script with command "kafka-2.3.0/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test" in window 1
+    end tell
+EOF
+
